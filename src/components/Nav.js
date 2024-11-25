6ce0d250dff64e758320dbff339dbdcc8d98bec2
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-25 18:22:10 +0300

    Nav Edited

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,39 +8,50 @@ function Nav() {
       id="mainNav"
     >
       <div className="container">
-        <NavLink to="/" className="navbar-brand" href="#">
-          Hike
-        </NavLink>
-
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item mx-0 mx-lg-1">
-              <NavLink
-                to="/"
-                className="nav-link py-3 px-0 px-lg-3 rounded"
-                style={({ isActive }) => {
-                  return {
-                    textDecoration: isActive ? "underline" : "none",
-                  };
-                }}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item mx-0 mx-lg-1">
-              <NavLink
-                to="/trips"
-                className="nav-link py-3 px-0 px-lg-3 rounded"
-                style={({ isActive }) => {
-                  return {
-                    textDecoration: isActive ? "underline" : "none",
-                  };
-                }}
-              >
-                Trips
-              </NavLink>
-            </li>
-          </ul>
+        <img src="" alt="Bank-Logo"></img>
+        <div>
+          <NavLink to="/" className="navbar-brand" href="#">
+            Home
+          </NavLink>
+          <li className="nav-item mx-0 mx-lg-1">
+            <NavLink
+              to="/transactions"
+              className="nav-link py-3 px-0 px-lg-3 rounded"
+              style={({ isActive }) => {
+                return {
+                  textDecoration: isActive ? "underline" : "none",
+                };
+              }}
+            >
+              Transactions
+            </NavLink>
+          </li>
+          <li className="nav-item mx-0 mx-lg-1">
+            <NavLink
+              to="/users"
+              className="nav-link py-3 px-0 px-lg-3 rounded"
+              style={({ isActive }) => {
+                return {
+                  textDecoration: isActive ? "underline" : "none",
+                };
+              }}
+            >
+              Users
+            </NavLink>
+          </li>
+          <li className="nav-item mx-0 mx-lg-1">
+            <NavLink
+              to="/profile"
+              className="nav-link py-3 px-0 px-lg-3 rounded"
+              style={({ isActive }) => {
+                return {
+                  textDecoration: isActive ? "underline" : "none",
+                };
+              }}
+            >
+              Profile
+            </NavLink>
+          </li>
         </div>
       </div>
     </nav>
