6e54e805f897df949f5ba1011b6a76e31439ed30
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-27 19:53:38 +0300

    Users Shown

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { getAllUsers } from "../API/auth";
+import { useQuery } from "@tanstack/react-query";
+import UserItem from "./UserItem";
 
 const Users = () => {
+  const { data } = useQuery({
+    queryFn: getAllUsers,
+    queryKey: ["users"],
+  });
+
+  const userList = data?.map((user) => <UserItem user={user} />);
   return (
-    <div>
-      <div className="div-user"> user</div>
-      <div className="div-user"> user</div>
+    <div className="bg-[#F9E3BE] flex flex-col justify-center items-center ">
+      <div className=" flex flex-col flex-wrap md:flex-row gap-[20px] w-[76vw]  justify-center items-center mb-[50px]">
+        {userList}
+      </div>
     </div>
   );
 };
