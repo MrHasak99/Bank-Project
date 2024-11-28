5a98e8e88886affd14626034045b3fcf85cd1b32
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-28 19:47:59 +0300

    Fixed Item boxes

--- src/components/TransactionItems.js
+++ src/components/TransactionItems.js
@@ -3,6 +3,7 @@ const TransactionItems = ({ transaction }) => {
     <div>
       <div className="div-trans">
         <h1>{transaction.amount}</h1>
+        <h2>{transaction.type}</h2>
       </div>
     </div>
   );
